74a5b8abf47e6301cff52ad0eaf2c37cdf718eec
Khalil Mansour <mank3002@usherbrooke.ca>
2019-09-17 15:31:16 -0400

    test mapping gitlab cli

--- env_mapper.py
+++ env_mapper.py
@@ -1,15 +1,34 @@
 import json
+import sys
+import pprint
+
 
 def get_json(path):
-	#open the json file
+	# open the json file
 	with open(path, "r") as f:
 		datastore = json.load(f)
 	return datastore
 
-def main():
-	data = get_json("Web.Api/appsettings.json")
-	
+def map_values(params, data):
+	for key, value in params:
+		data["env"][key] = value
 
+def post_json(path, datastore):
+	# write to json file
+	with open(path, "w") as f:
+		json.dump(datastore, f, indent=4)
+
+def main(**kwargs):
+	# path
+	path = "Web.Api/appsettings.json"
+	# fetch json data
+	data = get_json(path)
+	# get all  kvp params
+	params = list(kwargs.items())
+	# map the values
+	map_values(params, data)
+	# update the file
+	post_json(path, data)
 
 if __name__ == "__main__":
-	main()
+	main(**dict(arg.split('=') for arg in sys.argv[1:]))
