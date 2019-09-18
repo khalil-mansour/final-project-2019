0bc07044c0d86efcfd7750ac65c7049769d0f6c0
Khalil Mansour <mank3002@usherbrooke.ca>
2019-09-18 10:17:29 -0400

    testing gitlab ci with python pt 6

--- env_mapper.py
+++ env_mapper.py
@@ -1,7 +1,9 @@
 import json
 import sys
 import pprint
-
+import os
+import configparser
+from io import StringIO
 
 def get_json(path):
 	# open the json file
@@ -9,27 +11,34 @@ def get_json(path):
 		datastore = json.load(f)
 	return datastore
 
-def map_values(params, data):
-	for key, value in params:
-		data["env"][key] = value
+def map_values(data, parser):
+	for option in parser['CONNECTIONSTRING']:
+		data["env"][option] = parser['CONNECTIONSTRING'][option]
 
 def post_json(path, datastore):
 	# write to json file
 	with open(path, "w") as f:
 		json.dump(datastore, f, indent=4)
 
-def main(**kwargs):
+def get_values():
+	with open('config.ini', 'r') as config_file:
+		config_text = os.path.expandvars(config_file.read())
+	config = configparser.ConfigParser()
+	config.readfp(StringIO(config_text))
+	return config
+
+def main():
+	# read ini file
+	parser = get_values()
 	# path
 	path = "Web.Api/appsettings.json"
 	# fetch json data
 	data = get_json(path)
-	# get all  kvp params
-	params = list(kwargs.items())
 	# map the values
-	map_values(params, data)
+	map_values(data, parser)
 	# update the file
 	post_json(path, data)
+	print(data)
 
 if __name__ == "__main__":
-	print(sys.argv[1:])
-	main(**dict(arg.split('::') for arg in sys.argv[1:]))
+	main()
