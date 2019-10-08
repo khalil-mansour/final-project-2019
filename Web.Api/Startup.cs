dd7ea2227566773ef36accc4ba6f7d9fb4d8728f
Philippe Courteau <coup2307@usherbrooke.ca>
2019-10-08 16:18:34 -0400

    added swager

--- Web.Api/Startup.cs
+++ Web.Api/Startup.cs
@@ -5,9 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
 using Web.Api.Core;
 using Web.Api.Infrastructure;
 using Web.Api.Presenters;
+
 
 namespace Web.Api
 {
@@ -31,7 +36,7 @@ namespace Web.Api
                     options.Authority = "https://securetoken.google.com/savvy-climber-252013";
                     options.TokenValidationParameters = new TokenValidationParameters
                     {
-                        ValidateIssuer =true,
+                        ValidateIssuer = true,
                         ValidIssuer = "https://securetoken.google.com/savvy-climber-252013",
                         ValidateAudience = true,
                         ValidAudience = "savvy-climber-252013",
@@ -42,11 +47,50 @@ namespace Web.Api
             InfrastructureConfigureServices.MapInfrastructureServices(services);
             CoreConfigureServices.MapCoreServices(services);
             services.AddSingleton<RegisterUserPresenter>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Hestia API",
+                    Description = "Structure of the api",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hestia API");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -59,9 +103,9 @@ namespace Web.Api
             }
 
             //app.UseHttpsRedirection();
-            
+
             app.UseMvc();
-            
+
         }
     }
 }
