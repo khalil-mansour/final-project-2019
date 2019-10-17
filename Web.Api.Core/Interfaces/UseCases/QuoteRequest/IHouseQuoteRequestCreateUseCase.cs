0aa09e1bd767138adc6a711134e9de5ce0440222
Philippe Courteau <coup2307@usherbrooke.ca>
2019-10-17 10:04:06 -0400

    wip

--- Web.Api.Core/Interfaces/UseCases/QuoteRequest/IHouseQuoteRequestCreateUseCase.cs
+++ Web.Api.Core/Interfaces/UseCases/QuoteRequest/IHouseQuoteRequestCreateUseCase.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Web.Api.Core.Dto.GatewayResponses.Repositories;
+using Web.Api.Core.Dto.UseCaseRequests.QuoteRequest;
+using Web.Api.Core.Dto.UseCaseResponses.QuoteRequest;
 
 namespace Web.Api.Core.Interfaces.UseCases.QuoteRequest
 {
-    public interface IHouseQuoteRequestCreateUseCase : IUseCaseRequestHandler<HouseQuoteCreateRequest, HouseQuoteRequestCreateRepoResponse>
-   {
+    public interface IHouseQuoteRequestCreateUseCase : IUseCaseRequestHandler<HouseQuoteCreateRequest, HouseQuoteCreateResponse>
+    {
 
     }
 }
