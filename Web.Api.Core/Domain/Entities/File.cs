8c4ae095a9922e5a6dfdcfac7dcc2fc0e19e1e31
Khalil Mansour <mank3002@usherbrooke.ca>
2019-10-28 13:56:16 -0400

    fixed FileResponse for multiple files

--- Web.Api.Core/Domain/Entities/File.cs
+++ Web.Api.Core/Domain/Entities/File.cs
@@ -6,25 +6,18 @@ namespace Web.Api.Core.Domain.Entities
     public class File
     {
 
-        [JsonProperty("uid")]
         public string UserId { get; }
 
-        [JsonProperty("document_type_id")]
         public int DocumentType { get; }
 
-        [JsonProperty("storage_file_id")]
         public string StorageId { get; }
 
-        [JsonProperty("created_date")]
         public DateTime CreatedDate { get; }
 
-        [JsonProperty("visiable")]
         public bool Visible { get; }
 
-        [JsonProperty("user_file_name")]
         public string FileName { get; }
 
-        [JsonProperty("url")]
         public string Url { get; set; }
 
         internal File(string userId, int documentType, string fileName, string storageId, DateTime createdDate, bool visible)
